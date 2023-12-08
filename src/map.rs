e0e497d3c59876d100e1adef2aa4e73eda735100
WaterFace <kevinvisserweckworth@gmail.com>
2023-12-08 06:38:25 -0500

    added regions to map

--- src/map.rs
+++ src/map.rs
@@ -87,6 +87,27 @@ impl PathNodeBundle {
     }
 }
 
+#[derive(Debug, Default, Component, Clone)]
+pub struct Region {
+    pub name: String,
+    pub area: Rect,
+}
+
+impl Region {
+    fn from_pixel_coords(name: String, top_left: Vec2, size: Vec2) -> Self {
+        let top_left = vec2(-MAP_SIZE / 2.0 + top_left.x, MAP_SIZE / 2.0 - top_left.y)
+            * physics::PHYSICS_SCALE
+            * MAP_SCALE;
+        Self {
+            name,
+            area: Rect::from_corners(
+                top_left,
+                top_left + size * physics::PHYSICS_SCALE * MAP_SCALE * vec2(1.0, -1.0),
+            ),
+        }
+    }
+}
+
 fn setup_map(mut commands: Commands, assets: Res<MapAssets>) {
     commands.spawn(SpriteBundle {
         sprite: Sprite {
@@ -150,11 +171,84 @@ fn setup_map(mut commands: Commands, assets: Res<MapAssets>) {
     commands.spawn(PathNodeBundle::from_pixel_coords(vec2(105.0, 430.0)));
     commands.spawn(PathNodeBundle::from_pixel_coords(vec2(268.0, 430.0)));
     commands.spawn(PathNodeBundle::from_pixel_coords(vec2(291.0, 430.0)));
+
+    commands.spawn(Region::from_pixel_coords(
+        "A".into(),
+        vec2(26.0, 18.0),
+        vec2(142.0, 166.0),
+    ));
+
+    commands.spawn(Region::from_pixel_coords(
+        "B".into(),
+        vec2(168.0, 142.0),
+        vec2(60.0, 42.0),
+    ));
+
+    commands.spawn(Region::from_pixel_coords(
+        "C".into(),
+        vec2(168.0, 18.0),
+        vec2(60.0, 28.0),
+    ));
+
+    commands.spawn(Region::from_pixel_coords(
+        "D".into(),
+        vec2(228.0, 18.0),
+        vec2(130.0, 166.0),
+    ));
+
+    commands.spawn(Region::from_pixel_coords(
+        "E".into(),
+        vec2(26.0, 184.0),
+        vec2(38.0, 74.0),
+    ));
+
+    commands.spawn(Region::from_pixel_coords(
+        "F".into(),
+        vec2(178.0, 184.0),
+        vec2(44.0, 94.0),
+    ));
+
+    commands.spawn(Region::from_pixel_coords(
+        "G".into(),
+        vec2(282.0, 184.0),
+        vec2(76.0, 82.0),
+    ));
+
+    commands.spawn(Region::from_pixel_coords(
+        "H".into(),
+        vec2(392.0, 128.0),
+        vec2(98.0, 138.0),
+    ));
+
+    commands.spawn(Region::from_pixel_coords(
+        "I".into(),
+        vec2(26.0, 256.0),
+        vec2(70.0, 216.0),
+    ));
+
+    commands.spawn(Region::from_pixel_coords(
+        "J".into(),
+        vec2(96.0, 278.0),
+        vec2(126.0, 64.0),
+    ));
+
+    commands.spawn(Region::from_pixel_coords(
+        "K".into(),
+        vec2(96.0, 399.0),
+        vec2(186.0, 73.0),
+    ));
+
+    commands.spawn(Region::from_pixel_coords(
+        "L".into(),
+        vec2(282.0, 266.0),
+        vec2(208.0, 206.0),
+    ));
 }
 
-fn debug_path_nodes(
+fn debug_path_nodes_and_regions(
     mut gizmos: Gizmos,
     query: Query<(&Transform, &PathNode)>,
+    region_query: Query<&Region, Without<PathNode>>,
     input: Res<Input<KeyCode>>,
     mut enabled: Local<bool>,
 ) {
@@ -166,6 +260,9 @@ fn debug_path_nodes(
         for (t, _node) in query.iter() {
             gizmos.circle_2d(t.translation.truncate(), 0.1, Color::RED);
         }
+        for region in region_query.iter() {
+            gizmos.rect_2d(region.area.center(), 0.0, region.area.size(), Color::TEAL);
+        }
     }
 }
 
@@ -173,7 +270,10 @@ impl Plugin for MapPlugin {
     fn build(&self, app: &mut App) {
         app.add_systems(Startup, load_map_assets)
             // TODO: do this when the game actually starts, and cleanup when resetting
-            .add_systems(PostStartup, setup_map)
-            .add_systems(Update, debug_path_nodes);
+            .add_systems(
+                Startup,
+                (apply_deferred, setup_map).chain().after(load_map_assets),
+            )
+            .add_systems(Update, debug_path_nodes_and_regions);
     }
 }
