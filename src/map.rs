d9a802ab91c1ee382d393a238d2ed5b0c742aa80
WaterFace <kevinvisserweckworth@gmail.com>
2023-12-07 23:48:40 -0500

    added nodes for pathfinding

--- src/map.rs
+++ src/map.rs
@@ -22,8 +22,8 @@ fn load_map_assets(mut commands: Commands, asset_server: Res<AssetServer>) {
     commands.insert_resource(MapAssets { texture })
 }
 
-const MAP_SCALE: f32 = 1.0;
-const MAP_SIZE: f32 = 512.0;
+pub const MAP_SCALE: f32 = 1.0;
+pub const MAP_SIZE: f32 = 512.0;
 
 #[derive(Debug, Default, Component)]
 pub struct Wall;
@@ -46,11 +46,39 @@ impl WallBundle {
                     -MAP_SIZE / 2.0 + top_left.x + size.x / 2.0,
                     MAP_SIZE / 2.0 - top_left.y - size.y / 2.0,
                     0.0,
-                ) * physics::PHYSICS_SCALE,
+                ) * physics::PHYSICS_SCALE
+                    * MAP_SCALE,
             ),
             collider: Collider::cuboid(
-                size.x / 2.0 * physics::PHYSICS_SCALE,
-                size.y / 2.0 * physics::PHYSICS_SCALE,
+                size.x / 2.0 * physics::PHYSICS_SCALE * MAP_SCALE,
+                size.y / 2.0 * physics::PHYSICS_SCALE * MAP_SCALE,
+            ),
+            global_transform: Default::default(),
+        }
+    }
+}
+
+#[derive(Debug, Default, Component)]
+pub struct PathNode;
+
+#[derive(Debug, Default, Bundle)]
+struct PathNodeBundle {
+    path_node: PathNode,
+    transform: Transform,
+    global_transform: GlobalTransform,
+}
+
+impl PathNodeBundle {
+    pub fn from_pixel_coords(position: Vec2) -> Self {
+        PathNodeBundle {
+            path_node: PathNode,
+            transform: Transform::from_translation(
+                vec3(
+                    -MAP_SIZE / 2.0 + position.x,
+                    MAP_SIZE / 2.0 - position.y,
+                    0.0,
+                ) * physics::PHYSICS_SCALE
+                    * MAP_SCALE,
             ),
             global_transform: Default::default(),
         }
@@ -65,8 +93,8 @@ fn setup_map(mut commands: Commands, assets: Res<MapAssets>) {
         },
         texture: assets.texture.clone(),
         transform: Transform::from_translation(vec3(
-            -MAP_SIZE / 2.0 * physics::PHYSICS_SCALE,
-            MAP_SIZE / 2.0 * physics::PHYSICS_SCALE,
+            -MAP_SIZE / 2.0 * physics::PHYSICS_SCALE * MAP_SCALE,
+            MAP_SIZE / 2.0 * physics::PHYSICS_SCALE * MAP_SCALE,
             -5.0,
         ))
         .with_scale(Vec3::splat(MAP_SCALE * physics::PHYSICS_SCALE)),
@@ -84,12 +112,66 @@ fn setup_map(mut commands: Commands, assets: Res<MapAssets>) {
     commands.spawn(WallBundle::new(vec2(490.0, 0.0), vec2(22.0, 512.0)));
     commands.spawn(WallBundle::new(vec2(358.0, 18.0), vec2(131.0, 110.0)));
     commands.spawn(WallBundle::new(vec2(358.0, 128.0), vec2(34.0, 138.0)));
+
+    commands.spawn(PathNodeBundle::from_pixel_coords(vec2(156.0, 31.0)));
+    commands.spawn(PathNodeBundle::from_pixel_coords(vec2(177.0, 31.0)));
+    commands.spawn(PathNodeBundle::from_pixel_coords(vec2(216.0, 31.0)));
+    commands.spawn(PathNodeBundle::from_pixel_coords(vec2(156.0, 31.0)));
+    commands.spawn(PathNodeBundle::from_pixel_coords(vec2(236.0, 31.0)));
+
+    commands.spawn(PathNodeBundle::from_pixel_coords(vec2(45.0, 164.0)));
+    commands.spawn(PathNodeBundle::from_pixel_coords(vec2(160.0, 164.0)));
+    commands.spawn(PathNodeBundle::from_pixel_coords(vec2(178.0, 164.0)));
+    commands.spawn(PathNodeBundle::from_pixel_coords(vec2(216.0, 164.0)));
+    commands.spawn(PathNodeBundle::from_pixel_coords(vec2(320.0, 164.0)));
+    commands.spawn(PathNodeBundle::from_pixel_coords(vec2(198.0, 177.0)));
+    commands.spawn(PathNodeBundle::from_pixel_coords(vec2(236.0, 164.0)));
+
+    commands.spawn(PathNodeBundle::from_pixel_coords(vec2(45.0, 193.0)));
+    commands.spawn(PathNodeBundle::from_pixel_coords(vec2(198.0, 193.0)));
+    commands.spawn(PathNodeBundle::from_pixel_coords(vec2(320.0, 193.0)));
+
+    commands.spawn(PathNodeBundle::from_pixel_coords(vec2(46.0, 249.0)));
+    commands.spawn(PathNodeBundle::from_pixel_coords(vec2(210.0, 268.0)));
+    commands.spawn(PathNodeBundle::from_pixel_coords(vec2(320.0, 245.0)));
+    commands.spawn(PathNodeBundle::from_pixel_coords(vec2(440.0, 253.0)));
+
+    commands.spawn(PathNodeBundle::from_pixel_coords(vec2(53.0, 268.0)));
+    commands.spawn(PathNodeBundle::from_pixel_coords(vec2(203.0, 285.0)));
+    commands.spawn(PathNodeBundle::from_pixel_coords(vec2(320.0, 275.0)));
+    commands.spawn(PathNodeBundle::from_pixel_coords(vec2(440.0, 275.0)));
+
+    commands.spawn(PathNodeBundle::from_pixel_coords(vec2(87.0, 314.0)));
+    commands.spawn(PathNodeBundle::from_pixel_coords(vec2(105.0, 314.0)));
+
+    commands.spawn(PathNodeBundle::from_pixel_coords(vec2(83.0, 430.0)));
+    commands.spawn(PathNodeBundle::from_pixel_coords(vec2(105.0, 430.0)));
+    commands.spawn(PathNodeBundle::from_pixel_coords(vec2(268.0, 430.0)));
+    commands.spawn(PathNodeBundle::from_pixel_coords(vec2(291.0, 430.0)));
+}
+
+fn debug_path_nodes(
+    mut gizmos: Gizmos,
+    query: Query<(&Transform, &PathNode)>,
+    input: Res<Input<KeyCode>>,
+    mut enabled: Local<bool>,
+) {
+    if input.just_pressed(KeyCode::Backslash) {
+        *enabled = !*enabled;
+    }
+
+    if *enabled {
+        for (t, _node) in query.iter() {
+            gizmos.circle_2d(t.translation.truncate(), 0.1, Color::RED);
+        }
+    }
 }
 
 impl Plugin for MapPlugin {
     fn build(&self, app: &mut App) {
         app.add_systems(Startup, load_map_assets)
             // TODO: do this when the game actually starts, and cleanup when resetting
-            .add_systems(PostStartup, setup_map);
+            .add_systems(PostStartup, setup_map)
+            .add_systems(Update, debug_path_nodes);
     }
 }
