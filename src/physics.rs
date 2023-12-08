c6c6a2874fe908dd2a85d91dfd4138d456045125
WaterFace <kevinvisserweckworth@gmail.com>
2023-12-07 21:24:20 -0500

    added physics debug switch, and prep for moving to sprites

--- src/physics.rs
+++ src/physics.rs
@@ -1,14 +1,25 @@
 use bevy::prelude::*;
 use bevy_rapier2d::prelude::*;
 
-pub struct PhysicsPlugin;
+pub struct PhysicsPlugin {
+    pub debug: bool,
+}
+
+pub const PHYSICS_SCALE: f32 = 1.0 / 32.0;
 
 impl Plugin for PhysicsPlugin {
     fn build(&self, app: &mut App) {
-        app.add_plugins(RapierPhysicsPlugin::<()>::default())
-            .insert_resource(RapierConfiguration {
-                gravity: Vec2::ZERO,
+        app.add_plugins((
+            RapierPhysicsPlugin::<()>::default(),
+            RapierDebugRenderPlugin {
+                enabled: self.debug,
+                mode: DebugRenderMode::COLLIDER_SHAPES,
                 ..Default::default()
-            });
+            },
+        ))
+        .insert_resource(RapierConfiguration {
+            gravity: Vec2::ZERO,
+            ..Default::default()
+        });
     }
 }
